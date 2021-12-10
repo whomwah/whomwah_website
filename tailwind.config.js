ae4d263d4542105af7c15284b34a22a9a59b1a38
Duncan Robertson <duncan@whomwah.com>
2021-12-10 09:51:27 +0000

    Tailwind 3 upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 module.exports = {
   mode: 'jit',
-  purge: [
-    'index.html'
+  content: [
+    '*.html'
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       'istok': ['Istok Web', 'sans-serif'],
     },
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
